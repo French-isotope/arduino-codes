2a3500fb10d50821c374f8d1c6a6da61522e13af
Isotope <frenchisotope@gmail.com>
2024-06-15 10:15:05 +0200

    add: code to check if modifying has worked

--- change_I2C_addresses.c
+++ change_I2C_addresses.c
@@ -16,11 +16,53 @@ void setup(){
 	
 	byte error = Wire.endTransmission();
 	
-	if (error == 0) {
+	if (error == 0) 
+	{
 		Serial.println("I2C address change command sent successfully. Waiting for device to rejoin the I2c bus...");
 		delay(1000);
-	} else {
+	} 
+	else 
+	{
 		Serial.println("Failed to send I2C command");
 	}
 }
 
+
+void loop() {
+	byte newAddress = 0x32; // New address to set, should be changed !!
+	
+	// verifying new address
+	Wire.beginTransmission(newAddress);
+	Wire.write(0x01); // WHO AM I command
+	byte error = Wire.endTransmission();
+	
+	if (error == 0) 
+	{
+		Wire.requestFrom((uint8_t)newAddress, (uint8_t)1);
+		if (Wire.available()) 
+		{
+			byte response = Wire.read();
+			if (response == 0xA1) 
+			{
+				Serial.print("I2C address successfully changed to : ");
+				Serial.println(newAddress);
+			} 
+			else 
+			{
+				Serial.print("Unexpected response: 0x");
+				Serial.println(response, HEX);
+			}
+		}
+		else
+		{
+			Serial.println("No response received");
+		}
+	}
+	else
+	{
+		Serial.println("Failed to communicate with the new address");
+		
+	}
+	delay(5000); // wait 5s before retry
+
+}
